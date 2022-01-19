b2b06d1efe9a1958a2faf1740e5e4b132807edfd
osvaldo gratereaux <osvaldo.nypl@gmail.com>
2022-01-18 21:42:26 -0500

    commit tree b6904022eebafa0ab18425d563e11e8130ac4da9

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word.split('').reverse().join('') === word ? true : false
 }
 
 /* 
   Add your pseudocode here
+  Reverses a string
+  if the reversed string 
+  is equal to the original string, 
+  then it returns true otherwise,
+  it returns false.
 */
 
 /*
   Add written explanation of your solution here
+  To reach the reversed string, 
+  first split the string into an 
+  array of the letters, reverse the letters, and
+  finally rejoin the letters to a string again.
 */
 
 // You can run `node index.js` to view these console logs
